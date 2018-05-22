9ff713c08ff3c94aa75efbe6febea1afd1ce6310
Amanda Oliver <amanda.oliver@williamhill.com>
2018-05-22 08:01:33 +0200

    cleaning after refactor

--- src/Components/Join.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-export default class Join extends Component {
-    render() {
-        return (
-            <button className='join'>Join us!</button>
-        )
-       
-    }
-}
